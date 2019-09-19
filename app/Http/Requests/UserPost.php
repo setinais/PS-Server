db9a28ef909b5c03f11247af926a1261b23590ac
unknown <Administrador@pso-labresi-08.ifto.local>
2019-09-18 22:52:56 -0300

    post

--- app/Http/Requests/UserPost.php
+++ app/Http/Requests/UserPost.php
@@ -44,26 +44,26 @@ class UserPost extends FormRequest
             'cpf.numeric' => 'O :attribute deve conter somente numeros!',
             'cpf.size'    => 'O :attribute tem que ter :size numeros!',
 
-            'name.required' => 'O :attribute é um campo Obrigatorio!',
-            'name.string'   => 'O :attribute deve conter somente caracteries!',
-            'name.max'      => 'O :attribute deve conter no maximo :max caracteries!',
+            'name.required' => 'O nome é um campo Obrigatorio!',
+            'name.string'   => 'O nome deve conter somente caracteries!',
+            'name.max'      => 'O nome deve conter no maximo :max caracteries!',
 
             'email.required' => 'O :attribute é um campo Obrigatorio!',
             'email.email'    => 'Este :attribute não é valido!',
             'email.max'      => 'O :attribute deve conter no maximo :max caracteries!',
             'email.unique'   => 'Já existe um :attribute cadastrado!',
 
-            'password.required'  => 'A :attribute é obrigatoria',
-            'password.string'    => 'A :attribute deve ser uma String!',
-            'password.min'       => 'A :attribute deve ter no minimo :min caracteries!',
+            'password.required'  => 'A senha é obrigatoria',
+            'password.string'    => 'A nome deve ser uma String!',
+            'password.min'       => 'A nome deve ter no minimo :min caracteries!',
 
             'sexo.in' => 'Valor do :attribute invalido!',
 
             'data_nascimento.required'  => 'A data de nacimento esta invalida!',
             'data_nascimento.date'      => 'A data de nacimento é Obrigatoria!',
 
-            'cartao_sus.required'=> 'O :attribute é obrigatorio!',
-            'cartao_sus.integer' => 'O :attribute deve conter somente numeros!',
+            'cartao_sus.required'=> 'O numero do cartâo SUS é obrigatorio!',
+            'cartao_sus.integer' => 'O numero do cartâo SUS deve conter somente numeros!',
             'cartao_sus.unique' => 'Já existe um cartao sus com este numero!',
 
         ];
--- app/User.php
+++ app/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable implements MustVerifyEmail
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'sexo', 'cartao_sus', 'data_nascimento', 'cpf'
+        'name', 'email', 'password',
     ];
 
     /**
